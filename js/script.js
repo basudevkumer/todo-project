8dddcb5942f82f28c24ce362deb7d88cf3d1b04a
basudevkumer <jhulonkumar40@gmail.com>
2025-06-23 14:45:03 +0600

    error solve

---
+++ js/script.js
@@ -0,0 +1,44 @@
+let nameInput = document.querySelector(".nameInput");
+let captionInput = document.querySelector(".captionInput");
+let postbtn = document.querySelector(".postbtn");
+let mainPost = document.querySelector(".mainPost");
+let crossOne = document.querySelector(".cross-one");
+let crossTwo = document.querySelector(".cross-two");
+
+let array = [];
+
+postbtn.addEventListener("click", function () {
+  if (!nameInput.value) {
+    crossOne.style.display = "block";
+  } else if (!captionInput.value) {
+    crossTwo.style.display = "block";
+  } else {
+    crossOne.style.display = "none";
+    crossTwo.style.display = "none";
+    array.push({
+      name: nameInput.value,
+      caption: captionInput.value,
+    });
+    display();
+
+    nameInput.value = "";
+    captionInput.value = "";
+  }
+});
+
+function display() {
+  mainPost.innerHTML = "";
+
+  array.map((items) => {
+    mainPost.innerHTML += ` <div class="card" style="width: 20rem">
+          <div class="card-body">
+            <h5 class="card-title">${items.name}</h5>
+            <p class="card-text">${items.caption}</p>
+           <div class="d-flex flex-cols  gap-4">
+             <button class="btn btn-success  upbtn">Update</button>
+            <button class="btn btn-danger deletebtn">Delete</button>
+           </div>
+          </div>
+        </div>`;
+  });
+}
